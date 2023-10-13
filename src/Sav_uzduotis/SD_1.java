2b791f1f9d5befbd24afa8393c11613e8935c47d
Audrius Ivko <audrius.ivko@gmail.com>
2023-10-13 22:28:14 +0300

    Initial commit

---
+++ src/Sav_uzduotis/SD_1.java
@@ -0,0 +1,90 @@
+/*
+Parašyti programą, kuri nustato,
+ar loterijos bilietas yra laimingas. Loterijos bilieto šešiaženklisnumeris
+laikomas laimingu, jei jo pirmų trijų
+Skaičių suma yra lygi paskutinių trijų
+skaičių sumai, o visiskaitmenys
+biliete yra skirtingi.
+1.
+Bilieto
+numerį perduokite komandinėje
+eilutėje. Pvz: 628745
+2.
+Generuokite skaicius tol, kol rasite
+pirmą
+laimingą skaičių beiatspausdinkite, kiek reikėjo bandymų.
+*/
+package Sav_uzduotis;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class SD_1 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        for (; ; ) {
+            System.out.println("Iveskite šešiaženklį sklaiciu, be pasikartojančiu skaičių: ");
+            String ticket_txt = inp.next();
+            if (isNumeric(ticket_txt) && ticket_txt.length() == 6) {
+                if (noDuplicate(ticket_txt)) {
+                    if (checkTickets(ticket_txt)) {
+                        System.out.println("Jusu bilieto skaicius laimingas! ");
+                        count = 0;
+                    } else {
+                        System.out.println("Deja Jusu bilieto skaicius nelaimejo! ");
+                        System.out.println();
+                        randTicket();
+                    }
+                } else System.out.print("Ivestame skaičiuja pasikartoja skaičius. ");
+            } else {
+                System.out.println("Ivestas ne šešiaženklis skaičius");
+            }
+        }
+    }
+
+    private static void randTicket() {
+        Random random = new Random();
+        int firsTrio = random.nextInt(100, 1000);
+        int secondTrio = random.nextInt(100, 1000);
+        String str = Integer.toString(firsTrio) + Integer.toString(secondTrio);
+        if (noDuplicate(str)) {
+            count++;
+            System.out.println("Bandymas Nr.: " + count + " - " + str);
+            if (checkTickets(str)) {
+                System.out.println();
+                System.out.println("Laimingas bandymas " + count + ", bilietas Nr.: " + str);
+            } else randTicket();
+        } else randTicket();
+    }
+
+    public static boolean isNumeric(String str) {
+        return str != null && str.matches("\\d*\\.?\\d+");
+    }
+
+    public static boolean noDuplicate(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            int number = Integer.parseInt(String.valueOf(str.charAt(i)));
+            for (int y = i + 1; y < str.length(); y++) {
+                if (number == Integer.parseInt(String.valueOf(str.charAt(y)))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkTickets(String str) {
+        int firsTrio = 0;
+        int secondTrio = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (i < 3) {
+                firsTrio = firsTrio + Integer.parseInt(String.valueOf(str.charAt(i)));
+            } else {
+                secondTrio = secondTrio + Integer.parseInt(String.valueOf(str.charAt(i)));
+            }
+        }
+        return firsTrio == secondTrio;
+    }
+
+    static int count = 0;
+}
