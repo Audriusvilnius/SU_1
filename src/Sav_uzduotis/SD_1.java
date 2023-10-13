f19a323dace832f7cab6e72e2e8bd9ed5012b837
Audrius Ivko <audrius.ivko@gmail.com>
2023-10-13 22:39:19 +0300

    First commit

--- src/Sav_uzduotis/SD_1.java
+++ src/Sav_uzduotis/SD_1.java
@@ -1,19 +1,3 @@
-/*
-Parašyti programą, kuri nustato,
-ar loterijos bilietas yra laimingas. Loterijos bilieto šešiaženklisnumeris
-laikomas laimingu, jei jo pirmų trijų
-Skaičių suma yra lygi paskutinių trijų
-skaičių sumai, o visiskaitmenys
-biliete yra skirtingi.
-1.
-Bilieto
-numerį perduokite komandinėje
-eilutėje. Pvz: 628745
-2.
-Generuokite skaicius tol, kol rasite
-pirmą
-laimingą skaičių beiatspausdinkite, kiek reikėjo bandymų.
-*/
 package Sav_uzduotis;
 
 import java.util.Random;
